364f113dd57f547b665084b66aeebd960782923c
Esteban Talavera <tala@mac.lan>
2026-05-12 11:30:32 -0700

    Properly add viewport export so mobile renders at device width

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,12 @@ export const metadata: Metadata = {
   },
 };
 
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 5,
+};
 export default function RootLayout({
   children,
 }: {
